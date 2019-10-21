4c76937550110b0c9361551f11f3dfb0ccf322f1
MatthewDtotheG <mdgarrett1984@gmail.com>
2019-10-21 09:36:57 -0400

    Continues work

--- OO.js
+++ OO.js
@@ -1,38 +1,47 @@
 class ShippingLabel {
   constructor(){
     this.methodOne()
-    this.methodTwo()
-    this.methodThree()
+    // this.methodTwo()
+    // this.methodThree()
   }
-  
+
   methodOne(){
     // I want to assign the values of the input array to the testObj keys
     const testObj = {
-      Shipment: "",
-      Number: "",
-      OrderNumebr: "",
-      Shipped: "",
-      FirstName: "",
-      LastName: "",
-      ParentShipment: "N/A",
+      Shipment: 0,
+      Number: 1, 
+      OrderNumber: 2, 
+      FirstName: 3, 
+      LastName: 4 
     };
-    const test = input.split('\n');
-    for(let i = 0;i < test.length; i++){
-      console.log(test[i].split(','));
-    }
     
-    for(let key in testObj){
-      console.log(key);
-    }  
+    const resultArr = []
+    const result = {}
+    const inputResult = input.split('\n')
+    
+    for(let item in inputResult){
+      const itemValues = inputResult[item].trim().split(',');
+      
+      for(let key in testObj){
+        const indexOfItems = itemValues.indexOf(itemValues[testObj[key]])
+        if(testObj[key] === indexOfItems){
+          result[key] = itemValues[testObj[key]]
+          //resultArr.push(result)
+        }
+      }
+      console.log(result);
+            
+    }
+    console.log(resultArr);
   }
-  
-  methodTwo(){
-    console.log('METHOD TWO');
-  }
-  methodThree(){
-    console.log('METHOD THREE')
-  }
-  
+
+  // methodTwo(){
+  //   console.log('METHOD TWO');
+  // }
+  // methodThree(){
+  //   console.log('METHOD THREE')
+  // }
+
 }
 
 // Shipment #1:
@@ -43,12 +52,17 @@ class ShippingLabel {
 // Last Name: Smith, 
 // Parent Shipment: N/A
 
-const input = `SH348503,O567843,2018-12-10 15:08:58 -0000,Jane,Smith,
-SH465980,O936726,2018-12-11 06:08:14 -0000,John,Reynolds,
-SH465994,O936726,2018-12-11 06:12:37 -0000,John,Reynolds,
-SH867263,O234934,2018-12-11 18:28:51 -0000,Rebecca,Jones,
-SH907346,,2018-12-12 21:12:28 -0000,Rebecca,Jones,SH867263
-SH927813,,2018-12-15 09:49:35 -0000,Rebecca,Jones,SH907346`
+const input = `SH348503,O567843,2018-12-10 15:08:58 -0000,Jane,Smith, 
+SH465980,O936726,2018-12-11 06:08:14 -0000,John,Reynolds, 
+SH465994,O936726,2018-12-11 06:12:37 -0000,John,Reynolds, 
+SH867263,O234934,2018-12-11 18:28:51 -0000,Rebecca,Jones`
 
 const newLabel = new ShippingLabel(input)
 
+
+// var keys = ['foo', 'bar', 'baz'];
+// var values = [11, 22, 33]
+// 
+// var result = {};
+// keys.forEach((key, i) => result[key] = values[i]);
+// console.log(result);
