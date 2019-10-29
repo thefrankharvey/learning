9edf24dfa79cb402c5510831d36f1e9af4688cf0
MatthewDtotheG <mdgarrett1984@gmail.com>
2019-10-28 22:00:09 -0400

    Preston's solution

--- OO.js
+++ OO.js
@@ -1,56 +1,61 @@
 class ShippingLabel {
-  constructor(){
-    this.methodOne()
+  constructor() {
+    this.methodOne();
   }
 
-  methodOne(){
-    const shipArr = [ 'Number', 'Order Number', 'Shipped', 'First Name', 'Last Name', 'Parent Shipment']
-    const resultObj = {}
-    const result = []
-    const finalResult = []
-    const sortedValue = input.split('\n')
-    for(let i = 0; i < sortedValue.length; i++){
-          result.push(sortedValue[i].split(','));
-      }
-    for(let item in result){
-      const itemArray = result[item]
-      
-      for(let i in itemArray){
-        if(itemArray[i] === '' || itemArray[i] === ' '){
-          itemArray[i] = 'N/A'
-        };
-        
-        // console.log(shipArr[i]);
-        // console.log(itemArray[i]);
-        
-        resultObj[shipArr[i]] = itemArray[i]
-        
-      }  
+  methodOne() {
+    const shipArr = [
+      "Number",
+      "Order Number",
+      "Shipped",
+      "First Name",
+      "Last Name",
+      "Parent Shipment"
+    ];
+    const resultObj = {};
+    const result = [];
+    const finalResult = [];
+    const sortedValue = input.split("\n");
+    for (let i = 0; i < sortedValue.length; i++) {
+      result.push(sortedValue[i].split(","));
     }
+    for (let item in result) {
+      const itemArray = result[item];
+
+      for (let i in itemArray) {
+        if (itemArray[i] === "" || itemArray[i] === " ") {
+          itemArray[i] = "N/A";
+        }
+        resultObj[shipArr[i]] = itemArray[i];
+        console.log(resultObj);
+        // console.log(resultObj);
+        //console.log(shipArr[i]);
+        // console.log(itemArray[i]);
+      }
     }
   }
+}
 
 // Shipment #1:
-// Number: SH348503, 
-// Order Number: O567843, 
-// Shipped: 2018-12-10 15:08:58 -0000, 
+// Number: SH348503,
+// Order Number: O567843,
+// Shipped: 2018-12-10 15:08:58 -0000,
 // First Name: Jane,
-// Last Name: Smith, 
+// Last Name: Smith,
 // Parent Shipment: N/A
 
-const input = `SH348503,O567843,2018-12-10 15:08:58 -0000,Jane,Smith, 
-SH465980,O936726,2018-12-11 06:08:14 -0000,John,Reynolds, 
-SH465994,O936726,2018-12-11 06:12:37 -0000,John,Reynolds, 
-SH867263,O234934,2018-12-11 18:28:51 -0000,Rebecca,Jones, 
-SH907346,,2018-12-12 21:12:28 -0000,Rebecca,Jones,SH867263 
-SH927813,,2018-12-15 09:49:35 -0000,Rebecca,Jones,SH907346`
+const input = `SH348503,O567843,2018-12-10 15:08:58 -0000,Jane,Smith,
+SH465980,O936726,2018-12-11 06:08:14 -0000,John,Reynolds,
+SH465994,O936726,2018-12-11 06:12:37 -0000,John,Reynolds,
+SH867263,O234934,2018-12-11 18:28:51 -0000,Rebecca,Jones,
+SH907346,,2018-12-12 21:12:28 -0000,Rebecca,Jones,SH867263
+SH927813,,2018-12-15 09:49:35 -0000,Rebecca,Jones,SH907346`;
 
-const newLabel = new ShippingLabel(input)
-
+const newLabel = new ShippingLabel(input);
 
 // var keys = ['foo', 'bar', 'baz'];
 // var values = [11, 22, 33]
-// 
+//
 // var result = {};
 // keys.forEach((key, i) => result[key] = values[i]);
 // console.log(result);
---
+++ oop.js
@@ -0,0 +1,64 @@
+// // output (method one)
+// prettyPrintInput(input);
+
+class ShippingLabel {
+  constructor(input) {
+    this.input = input;
+    this.shipments = this.getShipmentsFromInput(input);
+  }
+
+  prettyPrintShipments() {
+    this.shipments.map((shipment, i) => {
+      console.log(
+        `Shipment #${i + 1}:\nNumber: ${shipment.number}, Order Number: ${
+          shipment.orderNumber
+        }, Shipped: ${shipment.shipped}, First Name: ${
+          shipment.firstName
+        }, Last Name: ${shipment.lastName}, Parent Shipment: ${
+          shipment.parentShipment
+        }`
+      );
+    });
+  }
+
+  // takes a string and returns an array of shipment objects
+  getShipmentsFromInput(input) {
+    return input.split("\n").map(line => {
+      const valOrPlaceholder = val => (val.trim() === "" ? "N/A" : val);
+
+      const [
+        number,
+        orderNumber,
+        shipped,
+        firstName,
+        lastName,
+        parentShipment
+      ] = line.split(",").map(val => valOrPlaceholder(val));
+
+      return {
+        number,
+        orderNumber,
+        shipped,
+        firstName,
+        lastName,
+        parentShipment
+      };
+    });
+  }
+}
+
+function run() {
+  const input = `SH348503,O567843,2018-12-10 15:08:58 -0000,Jane,Smith,
+  SH465980,O936726,2018-12-11 06:08:14 -0000,John,Reynolds,
+  SH465994,O936726,2018-12-11 06:12:37 -0000,John,Reynolds,
+  SH867263,O234934,2018-12-11 18:28:51 -0000,Rebecca,Jones,
+  SH907346,,2018-12-12 21:12:28 -0000,Rebecca,Jones,SH867263
+  SH927813,,2018-12-15 09:49:35 -0000,Rebecca,Jones,SH907346`;
+
+  const label = new ShippingLabel(input);
+
+  // Method One
+  label.prettyPrintShipments();
+}
+
+run();
